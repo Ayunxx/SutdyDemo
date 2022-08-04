5335b8824e66b1cb457c379bdc80f60e027aae9c
Ayunxxx <879654498@qq.com>
2022-08-04 22:22:27 +0800

    暂停 修正 45

--- gulimall-member/src/main/java/com/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,18 @@ package com.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+/*
+* 1、想要远程调用别的服务
+*   引入openfeign
+*   编写一个接口，告诉springcloud这个接口需要调用远程服务
+*   声明接口的没你一个方法都是调用的哪个远程服务的哪个请求
+* 开启远程调用功能
+* */
+
+@EnableFeignClients(basePackages = "com.gulimall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
